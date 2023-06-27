2d209cdbf87238b0291c3319cf20c5e03f51e7af
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-06-27 12:25:28 -0400

    Style locale switcher

--- src/components/Configuration.jsx
+++ src/components/Configuration.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import {ChoiceList, Popover, Button, Box} from '@shopify/polaris';
-import {ToolsMajor} from '@shopify/polaris-icons';
+import {GlobeMajor} from '@shopify/polaris-icons';
 import { LOCALES, useLocale } from '../locale';
 
 export default function Configuration({open, onOpen, onClose}) {
   const {locale, setLocale} = useLocale();
 
   return (
-    <Popover
-      activator={<Button onClick={onOpen} icon={ToolsMajor} outline />}
-      active={open}
-      onClose={onClose}
-      fixed
-      fullHeight
-      preferredAlignment="right"
-    >
-      <Box padding="4">
-        <ChoiceList
-          title="Locale"
-          choices={LOCALES.map(locale => ({label: locale, value: locale}))}
-          selected={locale}
-          onChange={(selected) => setLocale(selected[0])}
-          allowMultiple={false}
-        />
-      </Box>
-    </Popover>
+    <Box paddingInlineEnd="4">
+      <Popover
+        activator={<Button onClick={onOpen} icon={GlobeMajor} outline />}
+        active={open}
+        onClose={onClose}
+        fixed
+        fullHeight
+        preferredAlignment="right"
+      >
+        <Box padding="4">
+          <ChoiceList
+            title="Locale"
+            choices={LOCALES.map(locale => ({label: locale, value: locale}))}
+            selected={locale}
+            onChange={(selected) => setLocale(selected[0])}
+            allowMultiple={false}
+          />
+        </Box>
+      </Popover>
+    </Box>
   )
 }
