7248de385c0cf5161a0f9749097d58be5aec03fe
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-06-28 14:17:47 -0400

    Refactor config

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 
-function currentApproachConfig(locale) {
+function baseConfig() {
   return {
-    entry: "./src/current-approach/index.jsx",
-    output: {
-      filename: `[name].${locale}.js`,
-      chunkFilename: `[name].${locale}.js`,
-      publicPath: `/current-approach/${locale}/`,
-      path: path.resolve(__dirname, 'dist', 'current-approach', locale),
-    },
     resolve: { 
-      alias: {
-        '__translations': `./translations/${locale}.json`,
-      },
       extensions: [".js", ".jsx"],
     },
     module: {
@@ -32,9 +22,6 @@ function currentApproachConfig(locale) {
       ],
     },
     plugins: [
-      new DefinePlugin({
-        'PATH_BASENAME': JSON.stringify('/current-approach'),
-      }),
       new HtmlWebpackPlugin({
         templateContent: `
           <html>
@@ -52,49 +39,37 @@ function currentApproachConfig(locale) {
   };
 }
 
-function idealApproachConfig() {
-  return {
-    entry: "./src/ideal-approach/index.jsx",
-    output: {
-      filename: `[name].js`,
-      chunkFilename: `[name].js`,
-      publicPath: `/ideal-approach/`,
-      path: path.resolve(__dirname, 'dist', 'ideal-approach'),
-    },
-    resolve: { extensions: [".js", ".jsx"] },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
-            },
-          },
-        },
-      ],
-    },
-    plugins: [
-      new DefinePlugin({
-        'PATH_BASENAME': JSON.stringify('/ideal-approach'),
-      }),
-      new HtmlWebpackPlugin({
-        templateContent: `
-          <html>
-            <head>
-              <meta name="google" value="notranslate" />
-              <link rel="stylesheet" href="https://unpkg.com/@shopify/polaris@11.2.2/build/esm/styles.css" />
-            </head>
-            <body>
-              <div id="root"></div>
-            </body>
-          </html>
-        `
-      }),
-    ],
+function currentApproachConfig(locale) {
+  const config = baseConfig();
+  config.entry = "./src/current-approach/index.jsx";
+  config.output = {
+    filename: `[name].${locale}.js`,
+    chunkFilename: `[name].${locale}.js`,
+    publicPath: `/current-approach/${locale}/`,
+    path: path.resolve(__dirname, 'dist', 'current-approach', locale),
   };
+  config.resolve.alias = {
+    '__translations': `./translations/${locale}.json`,
+  };
+  config.plugins.push(new DefinePlugin({
+    'PATH_BASENAME': JSON.stringify('/current-approach'),
+  }));
+  return config;
+}
+
+function idealApproachConfig() {
+  const config = baseConfig();
+  config.entry = "./src/ideal-approach/index.jsx";
+  config.output = {
+    filename: `[name].js`,
+    chunkFilename: `[name].js`,
+    publicPath: `/ideal-approach/`,
+    path: path.resolve(__dirname, 'dist', 'ideal-approach'),
+  };
+  config.plugins.push(new DefinePlugin({
+    'PATH_BASENAME': JSON.stringify('/ideal-approach'),
+  }));
+  return config;
 }
 
 module.exports = [
