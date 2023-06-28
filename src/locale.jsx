f3edbe60ed8a899936acbd73cb2f902f0aebdd78
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-06-28 09:31:36 -0400

    Reload window on locale change

--- src/locale.jsx
+++ src/locale.jsx
@@ -7,7 +7,10 @@ export const LocaleContext = createContext({locale: 'en', setLocale: () => {}});
 export function LocaleProvider({children}) {
   const [cookies, setCookie] = useCookies(['locale']);
   const locale = cookies.locale ?? 'en';
-  const setLocale = useCallback((newLocale) => setCookie('locale', newLocale), [setCookie]);
+  const setLocale = useCallback((newLocale) => {
+    setCookie('locale', newLocale)
+    window.location.reload();
+  }, [setCookie]);
   return <LocaleContext.Provider value={{locale, setLocale}}>{children}</LocaleContext.Provider>;
 }
 
